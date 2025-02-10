5768460c92ce96c2a30e9104d0910bb683c7b075
pbio <10051819+paulbalaji@users.noreply.github.com>
2025-02-10 17:33:59 +0000

    feat: update base chain to celo

--- script/deployTemplate/base/DeployRootBase.s.sol
+++ script/deployTemplate/base/DeployRootBase.s.sol
@@ -7,7 +7,7 @@ contract DeployRootBase is XERC20RootDeployFixture {
     function setUp() public override {
         _params = XERC20RootDeployFixture.RootDeploymentParameters({
             tokenAdmin: 0xa7ECcdb9Be08178f896c26b7BbD8C3D4E844d9Ba,
-            rootToken: 0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85,
+            rootToken: 0xcebA9300f2b948710d2653dD7B07f33A8B32118C,
             outputFilename: "base.json"
         });
     }
