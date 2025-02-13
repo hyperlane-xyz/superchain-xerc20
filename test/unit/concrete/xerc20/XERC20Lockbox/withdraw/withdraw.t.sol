c9e43d42ae588e316a7848fc78ed57fe7c17184a
Yorke Rhodes <yorke@hyperlane.xyz>
2025-02-13 14:46:41 -0500

    Implement withdrawTo on XERC20 lockbox

--- test/unit/concrete/xerc20/XERC20Lockbox/withdraw/withdraw.t.sol
+++ test/unit/concrete/xerc20/XERC20Lockbox/withdraw/withdraw.t.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.8.19 <0.9.0;
 import "../XERC20Lockbox.t.sol";
 
 contract WithdrawUnitConcreteTest is XERC20LockboxTest {
-    function test_GivenAnyAmount() external {
+    function test_withdraw_GivenAnyAmount() external {
         // It should burn the amount of XERC20 tokens from the caller
         // It should transfer the amount of ERC20 tokens from the lockbox to the caller
         // It should emit a {Withdraw} event
@@ -27,6 +27,29 @@ contract WithdrawUnitConcreteTest is XERC20LockboxTest {
         assertEq(xVelo.balanceOf(users.alice), 0);
     }
 
+    function test_withdrawTo_GivenAnyAmount() external {
+        // It should burn the amount of XERC20 tokens from the caller
+        // It should transfer the amount of ERC20 tokens from the lockbox to the user
+        // It should emit a {Withdraw} event
+        uint256 amount = TOKEN_1 * 100_000;
+        deal(address(rewardToken), users.alice, amount);
+
+        vm.startPrank(users.alice);
+        rewardToken.approve(address(lockbox), amount);
+
+        lockbox.deposit(amount);
+
+        xVelo.approve(address(lockbox), amount);
+
+        vm.expectEmit(address(lockbox));
+        emit IXERC20Lockbox.Withdraw({_sender: users.alice, _amount: amount});
+        lockbox.withdrawTo(users.alice, amount);
+
+        assertEq(rewardToken.balanceOf(users.alice), amount);
+        assertEq(rewardToken.balanceOf(address(lockbox)), 0);
+        assertEq(xVelo.balanceOf(users.alice), 0);
+    }
+
     function testGas_withdraw() external {
         uint256 amount = TOKEN_1 * 100_000;
         deal(address(rewardToken), users.alice, amount);
@@ -39,5 +62,19 @@ contract WithdrawUnitConcreteTest is XERC20LockboxTest {
         xVelo.approve(address(lockbox), amount);
         lockbox.withdraw(amount);
         snapLastCall("XERC20Lockbox_withdraw");
+    }
+
+    function testGas_withdrawTo() external {
+        uint256 amount = TOKEN_1 * 100_000;
+        deal(address(rewardToken), users.alice, amount);
+
+        vm.startPrank(users.alice);
+        rewardToken.approve(address(lockbox), amount);
+
+        lockbox.deposit(amount);
+
+        xVelo.approve(address(lockbox), amount);
+        lockbox.withdrawTo(users.alice, amount);
+        snapLastCall("XERC20Lockbox_withdrawTo");
     }
 }
