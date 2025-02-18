42a9bc2d4dcf29e9bdea8441d1603fe524ccdeb1
Yorke Rhodes <yorke@hyperlane.xyz>
2025-02-18 18:19:17 -0500

    Add unit test of upgrade

--- test/unit/concrete/xerc20/XERC20Lockbox/XERC20Lockbox.t.sol
+++ test/unit/concrete/xerc20/XERC20Lockbox/XERC20Lockbox.t.sol
@@ -3,11 +3,24 @@ pragma solidity >=0.8.19 <0.9.0;
 
 import "test/BaseFixture.sol";
 
+import "@openzeppelin5/contracts/proxy/transparent/ProxyAdmin.sol";
+
 abstract contract XERC20LockboxTest is BaseFixture {
     function testInitialState() public view {
         assertEq(address(lockbox.ERC20()), address(rewardToken));
         assertEq(address(lockbox.XERC20()), address(xVelo));
 
-        assertEq(Ownable(_admin()).owner(), users.owner);
+        assertEq(ProxyAdmin(_admin()).owner(), users.owner);
+    }
+
+    function test_upgrade(address lockbox) public {
+        address newImpl = address(new XERC20(lockbox));
+
+        assertNotEq(_implementation(), newImpl);
+
+        vm.prank(users.owner);
+        ProxyAdmin(_admin()).upgradeAndCall(ITransparentUpgradeableProxy(address(xVelo)), newImpl, "");
+
+        assertEq(_implementation(), newImpl);
     }
 }
