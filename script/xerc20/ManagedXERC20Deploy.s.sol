68a619963434053721f3d726df7e797c031f5171
Yorke Rhodes <yorke@hyperlane.xyz>
2025-02-18 15:40:02 -0500

    Add deploy script

---
+++ script/xerc20/ManagedXERC20Deploy.s.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity >=0.8.19 <0.9.0;
+
+import {Script} from "forge-std/src/Script.sol";
+import {ManagedXERC20Lockbox} from "src/xerc20/ManagedXERC20Lockbox.sol";
+
+import "forge-std/src/Console.sol";
+
+contract ManagedXERC20Deploy is Script {
+    address erc20 = vm.envAddress("ERC20");
+    address xerc20 = vm.envAddress("XERC20");
+    address manager = vm.envAddress("MANAGER");
+
+    function run() public {
+        vm.startBroadcast();
+        ManagedXERC20Lockbox lockbox = new ManagedXERC20Lockbox(xerc20, erc20);
+        lockbox.grantRole(lockbox.MANAGER(), manager);
+        vm.stopBroadcast();
+    }
+}
