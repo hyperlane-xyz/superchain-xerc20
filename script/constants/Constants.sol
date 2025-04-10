a2f8064d01f5826f91b32c6f8e8096dc1d0571be
pbio <10051819+paulbalaji@users.noreply.github.com>
2025-04-10 17:21:06 +0100

    deploy xerc20 to new chains

--- script/constants/Constants.sol
+++ script/constants/Constants.sol
@@ -4,7 +4,10 @@ pragma solidity >=0.8.19 <0.9.0;
 /// @notice Constants used by Create3 deployment scripts
 abstract contract Constants {
     // "STAGING"
-    bytes11 public constant XERC20_FACTORY_ENTROPY = 0x0000000000535441474947;
+    // bytes11 public constant XERC20_FACTORY_ENTROPY = 0x0000000000535441474947;
+
+    // PRODUCTION
+    bytes11 public constant XERC20_FACTORY_ENTROPY = 0x0000000000000001111111;
 
     // used by factory
     bytes11 public constant XERC20_ENTROPY = 0x0000000000000000000000;
