4870383db3d8055399f07f42a979d9fd6b398bbc
Paul Balaji <10051819+paulbalaji@users.noreply.github.com>
2025-04-24 18:56:44 +0100

    feat: deploy staging contracts (#24)

---
+++ script/deployParameters/linea/DeployBase.s.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.15;
+
+import {XERC20DeployFixture} from "../../xerc20/XERC20DeployFixture.s.sol";
+
+contract DeployBase is XERC20DeployFixture {
+    function setUp() public override {
+        _params = XERC20DeployFixture.DeploymentParameters({
+            tokenAdmin: 0xa7ECcdb9Be08178f896c26b7BbD8C3D4E844d9Ba,
+            outputFilename: "linea.json"
+        });
+    }
+}
